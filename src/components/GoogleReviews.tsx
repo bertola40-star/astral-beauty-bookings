d1aa3761f87f05835f539de27e61fd8b1adbaa61
bertola40-star <bertola40@gmail.com>
2026-06-06 07:35:32 -0400

    Update GoogleReviews.tsx

--- src/components/GoogleReviews.tsx
+++ src/components/GoogleReviews.tsx
@@ -47,7 +47,7 @@ const GoogleReviews = () => {
 
   const fetchGoogleReviews = async () => {
     try {
-      const { data, error } = await supabase.functions.invoke('gdynamic-endpoint');
+      const { data, error } = await supabase.functions.invoke('dynamic-endpoint');
       if (error) throw error;
       setGoogleData(data as GooglePlaceData);
     } catch (error) {
